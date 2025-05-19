8dd09e46934abe3342c3faa3c75b43a5af9909d0
Mustafa <smart1hashi@gmail.com>
2025-05-19 10:23:07 +0100

    + Koin extension function

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -7,7 +7,7 @@ fun main(args: Array<String>) {
 }
 
 fun Application.module() {
-
+    configureKoin()
     configureSerialization()
     configureMonitoring()
     configureRouting()
--- src/main/kotlin/Koin.kt
+++ src/main/kotlin/Koin.kt
@@ -5,10 +5,8 @@ import io.ktor.server.application.install
 import org.koin.ktor.plugin.Koin
 import org.koin.logger.slf4jLogger
 
-class Koin {
-    fun Application.configureKoin(){
-        install(Koin){
-            slf4jLogger()
-        }
+fun Application.configureKoin() {
+    install(Koin) {
+        slf4jLogger()
     }
 }
